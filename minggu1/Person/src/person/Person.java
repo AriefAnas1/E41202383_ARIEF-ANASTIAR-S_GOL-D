8468fe7266596098601338ed5de087334dd94d5f
AriefAnas1 <ariefanas085@gmail.com>
2021-03-15 20:58:39 +0700

    5a

---
+++ minggu1/Person/src/person/Person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Arief Anastiar S
+ */
+import java.util.Scanner;
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner input = new Scanner(System.in);
+         System.out.print("FName = ");
+         String frame = input.next();
+         
+         
+         System.out.print("LName = ");
+         String lname = input.next();
+         
+         
+    }    
+    
+}
