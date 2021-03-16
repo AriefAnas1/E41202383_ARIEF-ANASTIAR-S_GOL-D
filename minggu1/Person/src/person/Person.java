713ca4eeac3ff4c0d9ebd836e9851dd09d6fb41b
AriefAnas1 <ariefanas085@gmail.com>
2021-03-16 17:35:47 +0700

    5a 5b

--- minggu1/Person/src/person/Person.java
+++ minggu1/Person/src/person/Person.java
@@ -18,14 +18,34 @@ public class Person {
     public static void main(String[] args) {
         // TODO code application logic here
          Scanner input = new Scanner(System.in);
-         System.out.print("FName = ");
-         String frame = input.next();
+         System.out.println("FName = ");
+         String fname = input.next();
          
          
-         System.out.print("LName = ");
+         System.out.println("LName = ");
          String lname = input.next();
          
          
+         System.out.println("Studentid = ");
+         String stuld = input.next();
+         
+         
+         System.out.println("Status = ");
+         String stuStatus = input.next();
+         
+         
     }    
+
+    Person(String arial, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean getAge() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
